5591a0fd32d1aa4b4ea4854514f38dc487654227
joezhu1230 <joezhu1230@gmail.com>
2022-04-27 17:24:53 -0700

    added junit and files

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo.minus(2, 1));
+    }
+}
+
